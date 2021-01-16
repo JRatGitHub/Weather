b1b55cfd14459de75ca17f1b4265b4ea31d647e3
JRatGitHub <janroelof@dewit-qian.nl>
2021-01-16 20:31:13 +0100

    Update module.php

--- Buienradar/module.php
+++ Buienradar/module.php
@@ -7,6 +7,10 @@
 			parent::Create();
 
 			$this->RegisterPropertyString ('City','Groningen');
+
+			$TEMPERATURE = $this->RegisterVariableFloat('TEMPERATURE','Temperatuur','~Temperature');
+			$HUMIDITY = $this->RegisterVariableInteger('HUMIDITY','uchtvochtigheid','~Intensity.100');
+			$AIRPRESSURE = $this->RegisterVariableFloat('AIRPRESSURE','Luchtdruk','~AirPressure.F');
 		}
 
 		public function Destroy()
